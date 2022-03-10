d0d528b170546b92960087acf0485d66af0be18d
danil.bikbulatov@antifreeze.games <danil.bikbulatov@antifreeze.games>
2022-03-10 18:41:02 +0500

    Задание 5 Создал базовый класс для тестов

---
+++ addressbook-web-tests/Tests/ContactCreationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests : ContactsTestBase
+    {
+        
+        [Test]
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            InitNewContactCreation();
+            ContactData contact = new ContactData("Bob");
+            contact.LastName = "Swarovski";
+            contact.MiddleName = "";
+            FillContactForm(contact);
+            SubmitContactCreation();
+            ReturnToHomePage();
+            Logout();
+        }
+
+        
+
+       
+
+        
+
+        
+
+      
+
+       
+
+        
+
+    }
+}
---
+++ addressbook-web-tests/Tests/ContactRemovalTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactRemovalTests : ContactsTestBase
+    {
+        [Test]
+        public void ContactRemovalTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToHomePage();
+            SelectContact(1);
+            RemoveContact();           
+        }
+
+
+        
+
+       
+
+      
+
+
+
+
+
+    }
+}
+
+
